4ab60c60421696d72c07776cd081ef895d75a39b
Mohamed-bip <97035858+Mohamed-bip@users.noreply.github.com>
2023-01-02 13:44:28 +0100

    Delete footer.view.php

--- footer.view.php
+++
@@ -1,5 +0,0 @@
-<?php if($showcookie) { ?>
-<div class="cookie-alert">
-   En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous proposer des contenus et services adaptés à vos centres d’intérêts.<br /><a href="php/accept_cookie.php">OK</a>
-</div>
-<?php } ?>
